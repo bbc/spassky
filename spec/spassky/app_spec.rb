2e898b17d45ca4efabed3b2d70e47ad6fe89a8ef
joshski <joshuachisholm@gmail.com>
2011-08-08 09:58:10 +0100

    removed capybara from app spec (now uses only rack test)

--- spec/spassky/app_spec.rb
+++ spec/spassky/app_spec.rb
@@ -9,11 +9,6 @@ require 'spassky/app'
 module Spassky
   describe App do
     include Rack::Test::Methods
-    include Capybara::DSL
-    
-    before do
-      Capybara.app = App
-    end
     
     def app
       App
@@ -21,19 +16,20 @@ module Spassky
     
     describe "GET /device/connect" do
       it "redirects to a unique URL" do
-        RandomStringGenerator.should_receive(:random_string).and_return("random-string", "random-string")
-        visit "/device/connect"
-        page.current_url.should == "http://www.example.com/device/idle/random-string"
+        RandomStringGenerator.should_receive(:random_string).and_return("random-string")
+        get "/device/connect"
+        last_response.should be_redirect
+        last_response.location.should == "http://example.org/device/idle/random-string"
       end
     end
     
     describe "GET /device/idle/123" do
       it "serves HTML page with a meta-refresh tag" do
         RandomStringGenerator.should_receive(:random_string).and_return("next-iteration")
-        visit "/device/idle/123"
+        get "/device/idle/123"
         seconds = 1
         url = "/device/idle/next-iteration"
-        page.html.should include("<meta http-equiv=\"refresh\" content=\"#{seconds}; url='#{url}'\">")
+        last_response.body.should include("<meta http-equiv=\"refresh\" content=\"#{seconds}; url='#{url}'\">")
       end
     end
     
