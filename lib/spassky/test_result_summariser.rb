b40d0bd7fbe78b7bc7bfdc63e769a4473326dfdd
Andrew Vos <andrew.vos@gmail.com>
2011-09-02 11:26:54 +0100

    finished off test result summariser

--- lib/spassky/test_result_summariser.rb
+++ lib/spassky/test_result_summariser.rb
@@ -1,7 +1,22 @@
 module Spassky
   class TestResultSummariser
+    def initialize device_statuses
+      @device_statuses = device_statuses
+    end
+
     def summary
-      "2 passed"
+      statuses = []
+      passes = status_count "pass"
+      fails = status_count "fail"
+      timeouts = status_count "timed out"
+      statuses << "#{passes} passed" if passes > 0
+      statuses << "#{fails} failed" if fails > 0
+      statuses << "#{timeouts} timed out" if timeouts > 0
+      statuses.join ", "
+    end
+
+    def status_count status
+      @device_statuses.find_all { |device_status| device_status.status == status }.size
     end
   end
 end
--- spec/spassky/test_result_summariser_spec.rb
+++ spec/spassky/test_result_summariser_spec.rb
@@ -4,10 +4,38 @@ module Spassky
   describe TestResultSummariser do
     it "shows passing tests" do
       device_statuses = [
-        Spassky::DeviceTestStatus.new('agent1', 'pass', 'test'),
-        Spassky::DeviceTestStatus.new('agent2', 'pass', 'test')
+        DeviceTestStatus.new('agent1', 'pass', 'test'),
+        DeviceTestStatus.new('agent2', 'pass', 'test')
       ]
       TestResultSummariser.new(device_statuses).summary.should == "2 passed"
+    end
+
+    it "shows failing tests" do
+      device_statuses = [
+        DeviceTestStatus.new('agent1', 'fail', 'test'),
+        DeviceTestStatus.new('agent2', 'fail', 'test')
+      ]
+      TestResultSummariser.new(device_statuses).summary.should == "2 failed"
+    end
+
+    it "shows timed out tests" do
+      device_statuses = [
+        DeviceTestStatus.new('agent1', 'timed out', 'test'),
+        DeviceTestStatus.new('agent2', 'timed out', 'test')
+      ]
+      TestResultSummariser.new(device_statuses).summary.should == "2 timed out"
+    end
+
+    it "shows failed, passed and timed out tests" do
+      device_statuses = [
+        DeviceTestStatus.new('agent1', 'pass', 'test'),
+        DeviceTestStatus.new('agent2', 'pass', 'test'),
+        DeviceTestStatus.new('agent3', 'fail', 'test'),
+        DeviceTestStatus.new('agent4', 'fail', 'test'),
+        DeviceTestStatus.new('agent5', 'timed out', 'test'),
+        DeviceTestStatus.new('agent6', 'timed out', 'test')
+      ]
+      TestResultSummariser.new(device_statuses).summary.should == "2 passed, 2 failed, 2 timed out"
     end
   end
 end
