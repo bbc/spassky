7b74cf42f1845957c8cb4fb928df40dc1707e95e
Aidy Lewis <aidy.lewis@googlemail.com>
2011-09-06 17:30:56 +0100

    refactored all methods to be below flog 15 measurement

--- lib/spassky/client/directory_reader.rb
+++ lib/spassky/client/directory_reader.rb
@@ -6,21 +6,25 @@ module Spassky::Client
     end
 
     def read_directory
+      Dir.glob(@pattern + "/**/*").inject({}) do |hash, path|
+        if File.file? path
+          key = path.gsub(/^#{@pattern}\//, "")
+          hash[key] = File.read(path)
+        end
+        hash
+      end
 
     end
 
+    def read_file
+      { @pattern => File.read(@pattern) }
+    end
 
     def read_files
       if File.file? @pattern
-        { @pattern => File.read(@pattern) }
+        read_file
       elsif File.directory? @pattern
-        Dir.glob(@pattern + "/**/*").inject({}) do |hash, path|
-          if File.file? path
-            key = path.gsub(/^#{@pattern}\//, "")
-            hash[key] = File.read(path)
-          end
-          hash
-        end
+        read_directory
       end
     end
   end
